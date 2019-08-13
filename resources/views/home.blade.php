8f5bc1570214d373c666cb9823c74d1b0e496e7f
Carlos Valdes <carlosv@simplemoviles.com>
2019-08-13 17:27:55 -0400

    new/contact page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <title>Xilo | Creative App Landing Pages HTML Templates</title>
+        <title>web publica</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
