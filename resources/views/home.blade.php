453fc7d0e580214ea674850e187ab3b6161a4313
Carlos Valdes <carlosv@simplemoviles.com>
2019-08-14 17:23:59 -0400

    new/footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -803,84 +803,10 @@
             <!-- Download end -->
         </main>
         <!-- Main Wrapper end -->
-        <!-- Footer start -->
-        <footer>
-            <div class="footer-area theme-bg pt-130 pb-80" id="footer">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xl-3 col-lg-3 col-md-6 mb-30">
-                            <div class="footer-widget">
-                                <div class="footer-address">
-                                    <address>394 University Ave.
-                                        Paolo Alto, CA 957236</address>
-                                </div>
-                                <div class="footer-cta">
-                                    <ul>
-                                        <li><a href="#">info@iSaascompany.com</a></li>
-                                        <li><a href="#">(+566)-699-9221</a></li>
-                                    </ul>
-                                </div>
-                                <div class="footer-social-info">
-                                    <ul>
-                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-2 col-lg-2 col-md-3 mb-30">
-                            <div class="footer-widget">
-                                <div class="footer-title">
-                                    <h5>Company</h5>
-                                </div>
-                                <div class="footer-menu">
-                                    <ul>
-                                        <li><a href="#">About Us</a></li>
-                                        <li><a href="#">Service</a></li>
-                                        <li><a href="#">Contact</a></li>
-                                        <li><a href="#">Price</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-2 col-lg-2 col-md-3 mb-30 d-none d-md-block">
-                            <div class="footer-widget">
-                                <div class="footer-title">
-                                    <h5>Support</h5>
-                                </div>
-                                <div class="footer-menu">
-                                    <ul>
-                                        <li><a href="#">Privacy</a></li>
-                                        <li><a href="#">Terms</a></li>
-                                        <li><a href="#">Faq</a></li>
-                                        <li><a href="#">Policy</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-5 col-lg-5 col-md-6 mb-30">
-                            <div class="footer-widget">
-                                <div class="footer-newsletter">
-                                    <h3>Subscribe our newsletter</h3>
-                                    <form action="">
-                                        <input type="text" placeholder="Email Address">
-                                        <i class="flaticon-send"></i>
-                                    </form>
-                                </div>
-                                <div class="footer-copyright">
-                                    <a href="index.html" class="footer-logo">Xilo</a>
-                                    <p>© 2018_FabiDesign_Lab</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- Footer end -->
+
+        @section('footer')
+            Footer
+        @endsection
 
 
 		<!-- All JS File here -->
